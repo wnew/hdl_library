97423a2406e818d7fca0c40a0a9e291b5d2813dd
wnew <wesley@ska.ac.za>
2018-11-13 11:45:06 +0200

    fixing bug in the count_to parameter
    A ^ was used instead of **

--- counter/counter.v
+++ counter/counter.v
@@ -15,10 +15,10 @@ module counter #(
       //==============================
       // Top level block parameters
       //==============================
-      parameter DATA_WIDTH   = 8,                // number of bits in counter
-      parameter COUNT_FROM   = 0,                // start with this number   
-      parameter COUNT_TO     = 2^(DATA_WIDTH-1), // value to count to in CL case
-      parameter STEP         = 1                 // negative or positive, sets direction
+      parameter DATA_WIDTH   = 8,               // number of bits in counter
+      parameter COUNT_FROM   = 0,               // start with this number
+      parameter COUNT_TO     = 2**(DATA_WIDTH), // value to count to in CL case
+      parameter STEP         = 1                // negative or positive, sets direction
    ) (
       //===============
       // Input Ports
@@ -26,23 +26,27 @@ module counter #(
       input clk,
       input en,
       input rst,
-      
+
       //===============
       // Output Ports
       //===============
-      output reg [DATA_WIDTH-1:0] out
+      output reg [DATA_WIDTH-1:0] count
    );
 
    // Synchronous logic
    always @(posedge clk) begin
+     // $display(DATA_WIDTH);
+     // $display(COUNT_TO);
+     // $display(COUNT_FROM);
+     // $display(STEP);
       // if ACTIVE_LOW_RST is defined then reset on a low
       // this should be defined on a system-wide basis
-      if ((`ifdef ACTIVE_LOW_RST rst `else !rst `endif) && out < COUNT_TO) begin
+      if ((`ifdef ACTIVE_LOW_RST rst `else !rst `endif) && count < COUNT_TO) begin
          if (en == 1) begin
-            out <= out + STEP;
+            count <= count + STEP;
          end
       end else begin
-         out <= COUNT_FROM;
+         count <= COUNT_FROM;
       end // else: if(rst != 0)
    end
 endmodule
--- counter/counter_tb.v
+++ counter/counter_tb.v
@@ -25,11 +25,11 @@ module counter_tb;
    reg clk;
    reg en;
    reg rst;
-   
+
    //==============
    // local wires
    //==============
-   wire [LOCAL_DATA_WIDTH-1:0] out;
+   wire [LOCAL_DATA_WIDTH-1:0] count;
 
    //=====================================
    // instance, "(d)esign (u)nder (t)est"
@@ -40,10 +40,10 @@ module counter_tb;
       .COUNT_TO     (`ifdef COUNT_TO     `COUNT_TO     `else 10           `endif),
       .STEP         (`ifdef STEP         `STEP         `else 1            `endif)
    ) dut (
-      .clk (clk), 
-      .en  (en), 
-      .rst (rst), 
-      .out (out)
+      .clk   (clk),
+      .en    (en),
+      .rst   (rst),
+      .count (count)
    );
 
    //=============
@@ -55,7 +55,7 @@ module counter_tb;
       en  = 1;
       rst = 0;
 
-      #50
+      #1000
       en = 0;
       #10
       en = 1;
@@ -71,11 +71,11 @@ module counter_tb;
    //===============
    // print output
    //===============
-   always @(posedge clk) $display(out);
-   
+   always @(posedge clk) $display(count);
+
    //===============================
    // finish after 100 clock cycles
    //===============================
-   initial #100 $finish;
+   initial #1000 $finish;
 
 endmodule
